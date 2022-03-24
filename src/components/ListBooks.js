b55f8542b2c0bbbcdba15b2ddce7a8f3b06d189e
Alexey <Leks11172@mail.ru>
2022-03-25 05:12:42 +0700

    sleep3

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Layout, Rate, Typography } from "antd";
+import { Card, Col, Layout, Rate, Row, Typography } from "antd";
 import { useNavigate } from "react-router-dom";
 
 const { Meta } = Card;
@@ -17,22 +17,32 @@ const ListBooks = ({ books = [] }) => {
 
   return (
     <>
+      {/* style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }} */}
       <Typography.Title>Library</Typography.Title>
-      <Layout style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }}>
-        {books.map((book) => (
-          <Card
-            className="card-hover"
-            key={book.title + book.author}
-            style={{ width: 300, marginTop: "16px", cursor: "pointer" }}
-            cover={<img style={{ width: 270 }} alt="example" src={book.cover} />}
-            onClick={() => navigation(`books/${book.genre}/${book.title}`)}
-          >
-            <Rate allowHalf defaultValue={calculateRate(book.rate)} disabled></Rate>
-            <p>Author: {book.author}</p>
-            <p>Genre: {book.genre}</p>
-            <Meta title={book.title} description={book.annotation} />
-          </Card>
-        ))}
+      <Layout>
+        <Row justify="center">
+          {books.map((book) => (
+            <Col
+              key={book.title + book.author}
+              xs={24}
+              md={12}
+              lg={8}
+              style={{ justifyContent: "center", display: "flex" }}
+            >
+              <Card
+                className="card-hover"
+                style={{ width: 300, marginTop: "16px", cursor: "pointer" }}
+                cover={<img style={{ width: 270 }} alt="example" src={book.cover} />}
+                onClick={() => navigation(`books/${book.genre}/${book.title}`)}
+              >
+                <Rate allowHalf defaultValue={calculateRate(book.rate)} disabled></Rate>
+                <p>Author: {book.author}</p>
+                <p>Genre: {book.genre}</p>
+                <Meta title={book.title} description={book.annotation} />
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Layout>
     </>
   );
